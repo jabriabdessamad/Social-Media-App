b8ef6ddec3519ecf1e5dbc318bf86ec9e6812f93
jabri abdessamad <jabriabdessamad2@gmail.com>
2022-09-09 17:41:31 +0100

    image picker

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:social_media_app/utils/colors.dart';
+import 'package:social_media_app/utils/utils.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({Key? key}) : super(key: key);
@@ -8,12 +12,99 @@ class AddPostScreen extends StatefulWidget {
 }
 
 class _AddPostScreenState extends State<AddPostScreen> {
+  Uint8List? _file;
+
+  _selectImage(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            title: const Text('Create a Post'),
+            children: [
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('take a photo'),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  Uint8List file = await pickImage(ImageSource.camera);
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              )
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: IconButton(
-      onPressed: () {},
-      icon: Icon(Icons.upload),
-    ));
+    // return Center(
+    //     child: IconButton(
+    //   onPressed: () {},
+    //   icon: Icon(Icons.upload),
+    // ));
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {},
+        ),
+        title: const Text('Post to'),
+        actions: [
+          TextButton(
+              onPressed: () {},
+              child: const Padding(
+                padding: EdgeInsets.only(right: 10),
+                child: Text(
+                  'Post',
+                  style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),
+                ),
+              ))
+        ],
+      ),
+      body: Column(children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://images.unsplash.com/photo-1506269996138-4c6d92fbd8a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwxMTU2OTE1fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.45,
+              child: TextField(
+                decoration: const InputDecoration(
+                  hintText: 'write a caption...',
+                  border: InputBorder.none,
+                ),
+                maxLines: 7,
+              ),
+            ),
+            SizedBox(
+              height: 45,
+              width: 45,
+              child: AspectRatio(
+                aspectRatio: 487 / 451,
+                child: Container(
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              'https://images.unsplash.com/photo-1506269996138-4c6d92fbd8a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwxMTU2OTE1fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'),
+                          fit: BoxFit.fill,
+                          alignment: FractionalOffset.topCenter)),
+                ),
+              ),
+            )
+          ],
+        ),
+      ]),
+    );
   }
 }
